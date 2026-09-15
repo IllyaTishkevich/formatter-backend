80e41c0d8ec869a757108bb3032823b25830a351
Illya Tsishkevich <relikt.ilya@gmail.com>
2026-09-16 01:50:16 +0300

    refactorin controller

--- src/Service/RequestInspector.php
+++ src/Service/RequestInspector.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 class RequestInspector
 {
     // never reflect these back, even to the same client that sent them
-    private const EXCLUDED_HEADERS = ['cookie', 'authorization', 'host'];
+    private const EXCLUDED_HEADERS = ['cookie', 'authorization', 'host', 'referer'];
 
     /**
      * @return array<string, mixed>
